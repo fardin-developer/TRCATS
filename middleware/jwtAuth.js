1e5a8e65b006d2a6bdefb58e101aece00afafa90
fardin379 <nrdsraz@gmail.com>
2024-02-28 09:54:26 +0530

    new routes added

--- middleware/jwtAuth.js
+++ middleware/jwtAuth.js
@@ -2,16 +2,22 @@ require('dotenv').config();
 
 const jwt = require('jsonwebtoken');
 function verifyToken(req, res, next) {
-const token = req.header('Authorization');
-const secretKey = "your-secret-key"
-if (!token) return res.status(401).json({ error: 'Access denied' });
-try {
- const decoded = jwt.verify(token, secretKey);
- req.userId = decoded.userId;
- next();
- } catch (error) {
- res.status(401).json({ error: 'Invalid token',status:"invalid token" });
- }
- };
+    const token = req.header('Authorization');
+    const secretKey = "your-secret-key"
+    if (!token) return res.status(401).json({
+        success: false,
+        message: 'Access denied'
+    });
+    try {
+        const decoded = jwt.verify(token, secretKey);
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        res.status(401).json({ 
+            success: false,
+            message: 'Invalid token',
+             status: "invalid token" });
+    }
+};
 
 module.exports = verifyToken;
