2d341dc4dcc3712e705cf776bfcd4bed46f942b6
fardin379 <nrdsraz@gmail.com>
2024-03-29 23:12:35 +0530

    faq true false added

--- Controller/faqController.js
+++ Controller/faqController.js
@@ -5,21 +5,30 @@ const FAQ = require('../Model/faqModel');
 exports.getFAQs = async (req, res) => {
   try {
     const faqs = await FAQ.find();
-    res.json(faqs);
+    res.json({
+      success: true,
+      data: faqs
+    });
   } catch (error) {
-    res.json({ message: error.message });
+    res.json({ success:false,
+      data:"server error"});
   }
 };
 
 exports.createFAQ = async (req, res) => {
   const { question, answer } = req.body;
   const faq = new FAQ({ question, answer });
-  console.log('ohk');
 
   try {
     const savedFAQ = await faq.save();
-    res.status(201).json(savedFAQ);
+    res.status(201).json({
+      success: true,
+      data: savedFAQ
+    });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({
+      success: false,
+      data: "error message faq"
+    });
   }
 };
